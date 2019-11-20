e8bbe794c42ce7f31911fc3aaea59974032a1aeb
Archil Karchava <archil1997@gmail.com>
2019-11-20 14:16:09 +0500

    fixed inability to log in

--- server/src/auth/dto/login.dto.ts
+++ server/src/auth/dto/login.dto.ts
@@ -1,7 +1,9 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   readonly email: string;
+  @IsString()
+  @IsNotEmpty()
   readonly password: string;
 }
